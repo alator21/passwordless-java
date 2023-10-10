e8c8385ef9bff10edd4bf82e161cb6d748490ed7
alator21 <botazz133@gmail.com>
2023-10-10 23:24:34 +0300

    Add dummy test file

---
+++ src/test/java/io/github/alator21/TestingTest.java
@@ -0,0 +1,16 @@
+package io.github.alator21;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+class TestingTest {
+
+
+    @Test
+    void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
